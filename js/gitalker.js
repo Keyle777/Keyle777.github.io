3d595ca887ee7c21e5a8c6649e4b55c10b08b518
Keyle777 <1059819521@qq.com>
2023-06-13 10:04:09 +0000

    Site updated: 2023-06-13 10:04:09

--- js/gitalker.js
+++ js/gitalker.js
@@ -1,9 +1,9 @@
 var gitalk = new Gitalk({
-    clientID: 'e9c8a4c42b9bd57da9d9',
-    clientSecret: '51b45e474eaf45a0b89f25e081da82a69fd47a09',
-    repo: 'lyxofficial.github.io',
-    owner: 'lyxofficial',
-    admin: ['lyxofficial'],
+    clientID: '6bcdc43a9d28279113c6',
+    clientSecret: 'ae259c5d3a63e62cfcd24194657c89e6921dd44b',
+    repo: 'keyle777.github.io',
+    owner: 'keyle777',
+    admin: ['keyle777'],
     id: location.pathname,      // Ensure uniqueness and length less than 50
     distractionFreeMode: false  // Facebook-like distraction free mode
 })
---
+++ js/gitcalendar.js
@@ -0,0 +1,8 @@
+ data: {
+	    user:'Keyle777', //用户名称
+	    fixed:'fixed',
+	    px:'px',
+	    x:'',
+            y:'',
+            span1:'',
+            span2:''}
